9db1e7ec6f925f771196b793d0c6789919917df1
mike <michaelwilsonsj@hotmail.com>
2021-05-13 17:16:24 -0400

    python conditional structures

---
+++ factory.py
@@ -0,0 +1,12 @@
+def main():
+    x, y = 1000, 100
+
+    if (x < y):
+        st = 'x is less than y'
+    else:
+        st = 'x is greater than y'
+
+    print(st)
+
+if __name__ == "__main__":
+    main()

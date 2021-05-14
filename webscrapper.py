a049d69dd4bad2b2d2bc87fb74690fcb150adeb3
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-14 16:57:28 -0400

    identify article titles no vote functionality

--- webscrapper.py
+++ webscrapper.py
@@ -5,4 +5,12 @@ res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.storylink')
 votes = soup.select('.score')
-print(votes[0])
+
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        hn.append(title)
+    return hn
+
+print(create_custom_hn(links,votes))

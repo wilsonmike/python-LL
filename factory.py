366fb8d560714e89d27532fe4f7969a9eeb31284
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-14 13:18:16 -0400

    fizzbuzz python && imported web scrapper (beautiful soup) (requests)

--- factory.py
+++ factory.py
@@ -13,6 +13,18 @@ def main():
     # ternary operator
     st = "x is less than y" if (x < y) else "x is greater than or equal to y"
     print(st)
+    
+    for num in range(0,100):
+       if num % 3 == 0 and num % 5 == 0:
+           print("FizzBuzz")
+       elif num % 3 == 0:
+           print("Fizz")
+       elif num % 5 == 0:
+            print("Buzz")
+       else:
+            print(num)
+    
+
 
 
 
---
+++ webscrapper.py
@@ -0,0 +1,4 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests('https://news.ycombinator.com/news')

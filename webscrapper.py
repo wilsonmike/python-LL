106b109ebf19d8a274cb327569d287c8bf2452c8
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-14 16:28:30 -0400

    add by bsoup functionality -- find id

--- webscrapper.py
+++ webscrapper.py
@@ -2,4 +2,5 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
-print(res)
+soup = BeautifulSoup(res.text, 'html.parser')
+print(soup.find(id='score_27153254'))

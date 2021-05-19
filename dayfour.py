94fe1a3f5c4ed302364374e0b781acb1bf1a17a4
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-19 12:03:09 -0400

    python dictionaries

--- dayfour.py
+++ dayfour.py
@@ -8,28 +8,28 @@
 
 # password generator
 
-import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-print('PyPassword Generator')
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input("How many symbols would you like in your password?\n"))
-nr_numbers = int(input("How many numbers would you like in your password?\n"))
+# import random
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+# print('PyPassword Generator')
+# nr_letters = int(input("How many letters would you like in your password?\n"))
+# nr_symbols = int(input("How many symbols would you like in your password?\n"))
+# nr_numbers = int(input("How many numbers would you like in your password?\n"))
 
-#Ez solution
-password = ""
+# #Ez solution
+# password = ""
 
-for char in range(1, nr_letters + 1):
-    password += random.choice(letters)
+# for char in range(1, nr_letters + 1):
+#     password += random.choice(letters)
     
-for char in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
+# for char in range(1, nr_symbols + 1):
+#     password += random.choice(symbols)
     
-for char in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
+# for char in range(1, nr_numbers + 1):
+#     password += random.choice(numbers)
 
-print(password)
+# print(password)
 
 #With randomization
 # password_list = []
@@ -44,3 +44,13 @@ print(password)
 #     password_list.append(random.choice(numbers))
 
 # print(password)
+
+#day 9 silent auction
+    #dictionaries in python
+
+programming_dictionary = {
+    "Go" : "Really nice syntax, fast, and sick logo.",
+    "React" : "Super in demand but frameworks are CRUD bound meh.",
+    "Python" : "Becoming a tool for everything| ML, AI, and even the web. +1"
+}
+print(programming_dictionary["React"])

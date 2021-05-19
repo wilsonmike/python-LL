9d64f697c488bb4c9369bd41ac8fbf258d28a021
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-19 10:35:48 -0400

    start of day 3 roller coaster

--- dayone.py
+++ dayone.py
@@ -10,14 +10,14 @@
 
 #day two tip calculator
 
-print("Welcome to the tip calculator")
-total_bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people are going to split the bill? "))
-bill_with_tip = tip / 100 * total_bill + total_bill
-total_bill = bill_with_tip / people
-round(total_bill, 2)
-print(f"Each person should pay {total_bill}")
+    # print("Welcome to the tip calculator")
+    # total_bill = float(input("What was the total bill? $"))
+    # tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+    # people = int(input("How many people are going to split the bill? "))
+    # bill_with_tip = tip / 100 * total_bill + total_bill
+    # total_bill = bill_with_tip / people
+    # round(total_bill, 2)
+    # print(f"Each person should pay {total_bill}")
 
 #fizzbuzz practice 
 
@@ -31,3 +31,7 @@ print(f"Each person should pay {total_bill}")
 #     else:
 #         print(num)
 
+
+# day 3 treasure console app
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))

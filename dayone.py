e9280d7459ed4c408d6ad7e3d22ce715b1900b17
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-19 10:21:58 -0400

    day 2 tip calculator

--- dayone.py
+++ dayone.py
@@ -7,3 +7,23 @@
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
 print("Your username could be " + city + " " + pet)
+
+#day two tip calculator
+
+print("Welcome to the tip calculator")
+total_bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+
+
+#fizzbuzz practice 
+
+# for num in range(0,20):
+#     if num % 3 == 0 and num % 5 == 0:
+#         print("FizzBuzz")
+#     elif num % 3 == 0:
+#         print("Fizz")
+#     elif num % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(num)
+

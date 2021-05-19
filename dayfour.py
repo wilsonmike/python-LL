7ff407b24cd04869dc90a00e76c49d4a9c1b4e8c
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-19 11:39:06 -0400

    password generator without randomness of choices

--- dayfour.py
+++ dayfour.py
@@ -13,4 +13,20 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 print('PyPassword Generator')
-input("How many letters would you in your password? ")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like in your password?\n"))
+nr_numbers = int(input("How many numbers would you like in your password?\n"))
+
+#Ez solution
+password = ""
+
+for char in range(1, nr_letters + 1):
+    password += random.choice(letters)
+    
+for char in range(1, nr_symbols + 1):
+    password += random.choice(symbols)
+    
+for char in range(1, nr_numbers + 1):
+    password += random.choice(numbers)
+
+print(password)

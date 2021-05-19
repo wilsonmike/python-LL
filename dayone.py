d5374ba9b3aa595aad28a2415dd0522aa7bbc485
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-19 09:29:29 -0400

    day one 100 days of python

---
+++ dayone.py
@@ -0,0 +1,11 @@
+# print("Hello, " + input("What is your name? "))
+# name = input("What is your favorite programming language? ")
+# print(name)
+
+# day one band name generator
+
+city = input("What's the name of the city you grew up in? ")
+print(city)
+pet = input("What's your pet's name? ")
+print(pet)
+print("Your username could be " + city + " " + pet)

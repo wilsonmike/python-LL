cb5042259578b520d973bd52c410bc8cf967f4be
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-19 09:33:31 -0400

    username generator completed

--- dayone.py
+++ dayone.py
@@ -4,8 +4,6 @@
 
 # day one band name generator
 
-city = input("What's the name of the city you grew up in? ")
-print(city)
-pet = input("What's your pet's name? ")
-print(pet)
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
 print("Your username could be " + city + " " + pet)

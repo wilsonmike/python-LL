952c2c4f6a9818f4e68a2ac28f1be46cff15be25
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-19 11:24:07 -0400

    starting lists for password generator

--- dayfour.py
+++ dayfour.py
@@ -1,7 +1,16 @@
-programming = ['React','Angular','C++','Python','Go']
-for items in programming:
-    print(items)
+# programming = ['React','Angular','C++','Python','Go']
+# for items in programming:
+#     print(items)
     
 
-for number in range(1,11):
-    print(number)
+# for number in range(1,11,3):
+#     print(number)
+
+# password generator
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+print('PyPassword Generator')
+input("How many letters would you in your password? ")
